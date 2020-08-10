c2581a85d4d5e5c79a09ba9fbe0261ede1495892
Jamiu Adam <jamiuadam120@gmail.com>
2020-08-10 03:40:12 +0100

    add functionality to force delete shopify shop model

--- src/ShopifyApp/Storage/Commands/Shop.php
+++ src/ShopifyApp/Storage/Commands/Shop.php
@@ -53,7 +53,7 @@ class Shop implements ShopCommand
         {
             if(ShopifyShop::where('user_id', session('shop'))->first())
             {
-                ShopifyShop::where('user_id', session('shop'))->delete();
+                ShopifyShop::where('user_id', session('shop'))->forceDelete();
             }
             $shop=User::find(session('shop'));
             $shop->shop_name = $domain->toNative();
@@ -72,7 +72,6 @@ class Shop implements ShopCommand
             $shop->save();
             session(['coming_from_shopify'=>$shop->id]);
         }
-
         return $shop->getId();
     }
 
